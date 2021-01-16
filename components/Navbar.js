c0037f18748b05ed241c062e0525fd2d162c74ac
Matt Mayfield <mattmayf411@gmail.com>
2021-01-16 09:19:05 -0600

    first mine

---
+++ components/Navbar.js
@@ -0,0 +1,107 @@
+import useDarkMode from 'use-dark-mode';
+import React, { useState } from 'react';
+
+export default function Navbar() {
+    const [navState, setNavState] = useState(false)
+
+    const darkModeConfig = {
+        classNameDark: 'dark',
+        classNameLight: 'light'
+    }
+    const darkMode = useDarkMode(false, darkModeConfig);
+
+    return (
+        <>
+            {/* Navbar Mobile*/}
+            <nav className={"lg:hidden flex-col fixed z-50 w-full dark:bg-matt-nav bg-white top-0 flex  px-2 py-3 shadow-lg " + (navState ? 'h-screen' : '') }>
+            <div className="container px-4 mx-auto w-full relative flex items-center justify-between ">
+                        <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase dark:text-gray-300 text-gray-600" href="/learning-lab/tailwind-starter-kit/presentation">
+                            Matthew Mayfield
+              </a>
+                        <button onClick={() => setNavState(!navState)} className="text-yellow-800 cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block outline-none focus:outline-none">
+                            {navState ? 
+                            <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                          </svg>
+                          :
+                          <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                            </svg>
+}
+                        </button>
+                    </div>
+                    <div className="flex-1 container px-4 mx-auto flex  items-center">
+                    
+                    {navState && <>
+                        <div className="flex flex-col h-full w-full" >
+                        <ul className="space-y-4 flex flex-col h-full justify-center items-center list-none ">
+                            <li className="px-3 py-2 flex items-center text-sm uppercase font-bold dark:text-gray-300 dark:hover:text-yellow-800 text-gray-600 hover:text-yellow-900 cursor-pointer"><span className="ml-2">About Me</span></li>
+                            <li className="px-3 py-2 flex items-center text-sm uppercase font-bold dark:text-gray-300 dark:hover:text-yellow-800 text-gray-600 hover:text-yellow-900 cursor-pointer"><span className="ml-2">Portfolio</span></li>
+                            <li className="px-3 py-2 flex items-center text-sm uppercase font-bold dark:text-gray-300 dark:hover:text-yellow-800 text-gray-600 hover:text-yellow-900 cursor-pointer"><span className="ml-2">Contact</span></li>
+                            <li className="px-3 py-2 flex items-center text-sm uppercase font-bold dark:text-gray-300 dark:hover:text-yellow-800 text-gray-600 hover:text-yellow-900 cursor-pointer"><span className="ml-2">Coffee</span></li>
+                            
+                        </ul>
+                        <div className="px-3 py-2 mx-auto text-xs uppercase font-bold dark:text-gray-300 dark:hover:text-yellow-800 text-gray-600 hover:text-yellow-900 cursor-pointer"><span className="ml-2"><div>
+                                {darkMode.value == true ?
+                                    <>
+                                        <button className="focus:outline-none text-2xl" onClick={darkMode.disable}>
+                                            ☀
+                                        </button> </>
+                                    :
+                                    <>
+                                        <button className="focus:outline-none text-2xl" onClick={darkMode.enable}>
+                                        🌙
+                                        </button> 
+                                        </>
+                                        }
+                            </div></span></div>
+                    </div>
+                    </> }
+                </div>
+            </nav>
+            {/* End Navbar Mobile*/}
+
+
+
+
+            {/* Navbar Desktop*/}
+            <nav className="hidden fixed z-50 w-full dark:bg-matt-nav bg-white top-0 lg:flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg shadow-lg">
+                <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+                    <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+                        <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase dark:text-gray-300 text-gray-600 " href="/learning-lab/tailwind-starter-kit/presentation">
+                            Matthew Mayfield
+              </a>
+                        <button onClick={() => setNavState(!navState)} className="text-yellow-800 cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none">
+                            <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                            </svg>
+                        </button>
+                    </div>
+                        <div className="lg:flex flex-grow items-center" >
+                        <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+                            <li className="px-3 py-2 flex items-center text-xs uppercase font-bold dark:text-gray-300 dark:hover:text-yellow-800 text-gray-600 hover:text-yellow-900 cursor-pointer"><span className="ml-2">About Me</span></li>
+                            <li className="px-3 py-2 flex items-center text-xs uppercase font-bold dark:text-gray-300 dark:hover:text-yellow-800 text-gray-600 hover:text-yellow-900 cursor-pointer"><span className="ml-2">Portfolio</span></li>
+                            <li className="px-3 py-2 flex items-center text-xs uppercase font-bold dark:text-gray-300 dark:hover:text-yellow-800 text-gray-600 hover:text-yellow-900 cursor-pointer"><span className="ml-2">Contact</span></li>
+                            <li className="px-3 py-2 flex items-center text-xs uppercase font-bold dark:text-gray-300 dark:hover:text-yellow-800 text-gray-600 hover:text-yellow-900 cursor-pointer"><span className="ml-2">Coffee</span></li>
+                            <li className="px-3 py-2 flex items-center text-xs uppercase font-bold dark:text-gray-300 dark:hover:text-yellow-800 text-gray-600 hover:text-yellow-900 cursor-pointer"><span className="ml-2"><div>
+                                {darkMode.value == true ?
+                                    <>
+                                        <button className="focus:outline-none text-2xl" onClick={darkMode.disable}>
+                                            ☀
+                                        </button> </>
+                                    :
+                                    <>
+                                        <button className="focus:outline-none text-2xl" onClick={darkMode.enable}>
+                                        🌙
+                                        </button> 
+                                        </>
+                                        }
+                            </div></span></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            {/* End Navbar Desktop*/}
+        </>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: ['./pages/**/*.js', './components/**/*.js'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'matt-dark': '#212121',
+        'matt-nav': 'rgba(33,33,33,0.95)'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
