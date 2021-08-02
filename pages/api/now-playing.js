360d683af9aa72af65d152040d654d7e7991b99a
Matt Mayfield <mattmayf411@gmail.com>
2021-08-02 12:05:16 -0500

    spotify tracks

--- pages/api/now-playing.js
+++ pages/api/now-playing.js
@@ -1,4 +1,4 @@
-import { getNowPlaying } from '../../lib/spotify';
+import { getNowPlaying } from "../../utils/spotify";
 
 export default async (_, res) => {
   const response = await getNowPlaying();
@@ -10,14 +10,14 @@ export default async (_, res) => {
   const song = await response.json();
   const isPlaying = song.is_playing;
   const title = song.item.name;
-  const artist = song.item.artists.map((_artist) => _artist.name).join(', ');
+  const artist = song.item.artists.map((_artist) => _artist.name).join(", ");
   const album = song.item.album.name;
   const albumImageUrl = song.item.album.images[0].url;
   const songUrl = song.item.external_urls.spotify;
 
   res.setHeader(
-    'Cache-Control',
-    'public, s-maxage=60, stale-while-revalidate=30'
+    "Cache-Control",
+    "public, s-maxage=60, stale-while-revalidate=30"
   );
 
   return res.status(200).json({
@@ -26,6 +26,6 @@ export default async (_, res) => {
     artist,
     isPlaying,
     songUrl,
-    title
+    title,
   });
 };
--- pages/api/recently-played.js
+++ pages/api/recently-played.js
@@ -1,36 +1,35 @@
-import { getRecentlyPlayed } from '../../lib/spotify';
+import { getRecentlyPlayed } from "../../utils/spotify";
 
 export default async (_, res) => {
   const response = await getRecentlyPlayed();
-console.log(response);
-//   if (response.status === 204 || response.status > 400) {
-//     return res.status(200).json({ isPlaying: false });
-//   }
+  console.log(response);
+  //   if (response.status === 204 || response.status > 400) {
+  //     return res.status(200).json({ isPlaying: false });
+  //   }
 
   const song = await response.json();
-//   const isPlaying = song.is_playing;
-//   const title = song.item.name;
-//   const artist = song.item.artists.map((_artist) => _artist.name).join(', ');
-//   const album = song.item.album.name;
-//   const albumImageUrl = song.item.album.images[0].url;
-//   const songUrl = song.item.external_urls.spotify;
+  //   const isPlaying = song.is_playing;
+  //   const title = song.item.name;
+  //   const artist = song.item.artists.map((_artist) => _artist.name).join(', ');
+  //   const album = song.item.album.name;
+  //   const albumImageUrl = song.item.album.images[0].url;
+  //   const songUrl = song.item.external_urls.spotify;
 
   res.setHeader(
-    'Cache-Control',
-    'public, s-maxage=60, stale-while-revalidate=30'
+    "Cache-Control",
+    "public, s-maxage=60, stale-while-revalidate=30"
   );
 
-//   return res.status(200).json({
-//     album,
-//     albumImageUrl,
-//     artist,
-//     isPlaying,
-//     songUrl,
-//     title
-//   });
+  //   return res.status(200).json({
+  //     album,
+  //     albumImageUrl,
+  //     artist,
+  //     isPlaying,
+  //     songUrl,
+  //     title
+  //   });
 
-
-return res.status(200).json({
-    song
+  return res.status(200).json({
+    song,
   });
 };
