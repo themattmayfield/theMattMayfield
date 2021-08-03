8b84e4d49e80245ba4c6bac021b22c2719137569
Matt Mayfield <mattmayf411@gmail.com>
2021-08-03 14:32:08 -0500

    scroll to top

--- components/UI/ScrollArrow.js
+++ components/UI/ScrollArrow.js
@@ -1,35 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { FaChevronUp } from "react-icons/fa";
-import { Link } from "react-scroll";
+import { BackTop } from "antd";
 
 const ScrollArrow = () => {
-  const [showScroll, setShowScroll] = useState(false);
-
-  const checkScrollTop = () => {
-    if (!showScroll && window.pageYOffset > 400) {
-      setShowScroll(true);
-    } else if (showScroll && window.pageYOffset <= 400) {
-      setShowScroll(false);
-    }
-  };
-  if (process.browser) {
-    window.addEventListener("scroll", checkScrollTop);
-  }
-  //   typeof window !== "undefined" &&
-  //   window.addEventListener("scroll", checkScrollTop);
-
   return (
-    <div
-      className={` fixed w-full bottom-5 items-center h-10 justify-end pr-2 ${
-        showScroll ? "flex" : "hidden"
-      }`}
-    >
-      <Link to="hero" spy={true} smooth={true}>
-        <div className="scrollTop cursor-pointer bg-matt-orange rounded-full w-6 h-6 flex items-center justify-center">
-          <FaChevronUp className="text-xs text-white dark:text-matt-textdark" />
-        </div>
-      </Link>
-    </div>
+    <BackTop>
+      <div className=" cursor-pointer bg-matt-orange rounded-full w-6 h-6 sm:h-8 sm:w-8 flex items-center justify-center">
+        <FaChevronUp className="text-xs text-white dark:text-matt-textdark" />
+      </div>
+    </BackTop>
   );
 };
 
