35f42ee3901cbf62818ff7f604ebc521feb5393e
Matt Mayfield <mattmayfield@Matt-Mayfield-MP7NQ02FY6.local>
2025-08-01 10:13:31 -0500

    sigh

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,10 @@ import { serve } from '@hono/node-server';
 import app from './index';
 
 const port = 3000;
-console.log(`Server is running on port ${port}`);
+console.log(`Server is running on port ${port}!`);
 
 serve({
   fetch: app.fetch,
   port,
 });
+
