f504d3278fddb05dc425ae3e0fbb08e0f3dc3cfd
Matt Mayfield <mattmayfield@Matt-Mayfield-MP7NQ02FY6.local>
2025-08-01 08:36:33 -0500

    miminal teat

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import { Hono } from 'hono';
-import { portfolioData } from './data/portfolio-data.js';
-import { renderPortfolio } from './templates/portfolio.js';
 
 const app = new Hono();
 
@@ -9,8 +7,7 @@ app.get('/favicon.ico', (c) => {
 });
 
 app.get('/', async (c) => {
-  const html = await renderPortfolio(portfolioData);
-  return c.html(html);
+  return c.html('<h1>Hello World</h1>');
 });
 
 export default app;
